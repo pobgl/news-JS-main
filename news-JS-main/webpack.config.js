6bbdd33d402d592a95f1c5f4a25e3b69c9acef84
pobgl <pglb@yandex.ru>
2022-07-03 18:05:44 +0400

    feat: add ts, webpack, eslint config

--- news-JS-main/webpack.config.js
+++ news-JS-main/webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const baseConfig = {
-    entry: path.resolve(__dirname, './src/index.js'),
+    entry: path.resolve(__dirname, './src/index.ts'),
     mode: 'development',
     module: {
         rules: [
@@ -15,10 +15,10 @@ const baseConfig = {
         ],
     },
     resolve: {
-        extensions: ['.js'],
+        extensions: ['.ts', '.tsx', '.js'],
     },
     output: {
-        filename: 'index.js',
+        filename: 'bundle-[hash].js',
         path: path.resolve(__dirname, '../dist'),
     },
     plugins: [
